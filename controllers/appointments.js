679bc97a7a18989065cbe1009576e5bdd83ed8e9
Cyrena Nielsen <cycheyenne@gmail.com>
2020-10-09 16:15:09 -0700

    set up toutes

---
+++ controllers/appointments.js
@@ -0,0 +1,5 @@
+const Appointment = require('../models/appointment');
+
+module.exports = {
+
+}
--- models/appointment.js
+++ models/appointment.js
@@ -1,14 +1,3 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
-const appointmentSchema = new Schema({
-    date: Date,
-    time: String,
-    service: String,
-    user: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    }]
-})
-
-module.exports = mongoose.model('Appointment', appointmentSchema)

a3787ba817ff2de33754848992275e052e4ec95c
Cyrena Nielsen <cycheyenne@gmail.com>
2020-10-09 16:24:26 -0700

    set up routes

--- routes/api/appointments.js
+++ routes/api/appointments.js
@@ -1,4 +1,4 @@
 
 const express = require('express');
 const router = express.Router();
-const scoresCtrl = require('../../controllers/appointment');
+const scoresCtrl = require('../../controllers/appointments');
---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+
+
+
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
+
+module.exports = router;

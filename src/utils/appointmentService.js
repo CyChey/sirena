029b5fd1207e17cc87a1e1b029bc3bc6af8da32b
Cyrena Nielsen <cycheyenne@gmail.com>
2020-10-09 20:23:12 -0700

    added pages and utils

---
+++ src/utils/appointmentService.js
@@ -0,0 +1,47 @@
+import tokenService from './tokenService';
+
+const BASE_URL = '/appointments/';
+
+// index
+export function getAllAppointments() {
+    return fetch(BASE_URL, {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${tokenService.getTokenFromLocalStorage()}`
+        }
+    })
+        .then(allAppointments => allAppointments.json());
+}
+
+// create
+export function createAppointment(appointmentToCreate) {
+    return fetch(BASE_URL, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${tokenService.getTokenFromLocalStorage()}`
+        },
+        body: JSON.stringify(appointmentToCreate)
+    }).then(newAppointment => newAppointment.json());
+}
+// delete
+export function deleteAppointmentAPI(appointmentIdToDelete) {
+    return fetch(`${BASE_URL}${appointmentIdToDelete}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${tokenService.getTokenFromLocalStorage()}`
+        }
+    }).then(deletedAppointment => deletedAppointment.json());
+}
+//
+export function updateAppointmentAPI(appointmentToUpdate) {
+    return fetch(`${BASE_URL}${appointmentToUpdate._id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${tokenService.getTokenFromLocalStorage()}`
+        },
+        body: JSON.stringify(appointmentToUpdate)
+    }).then(updatedAppointment => updatedAppointment.json());
+}

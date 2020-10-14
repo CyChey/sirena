1f30ea7ea77320d0662b757f9fc724aee05e3b36
Cyrena Nielsen <cycheyenne@gmail.com>
2020-10-14 13:53:53 -0700

    change casing

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import userService from '../../utils/userService';
-import './SignupForm.css'
+import './SignUpForm.css'
 
 class SignupForm extends Component {
 

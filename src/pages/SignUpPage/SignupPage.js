ed1eab969e174af8e9b7a22c86d0928e98203994
Cyrena Nielsen <cycheyenne@gmail.com>
2020-10-14 13:56:00 -0700

    casing

--- src/pages/SignUpPage/SignupPage.js
+++ src/pages/SignUpPage/SignupPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Helmet } from "react-helmet";
-import SignupForm from '../../components/SignUpForm/SignupForm';
+import SignupForm from '../../components/SignUpForm/SignUpForm';
 import Typography from '@material-ui/core/Typography';
 import './SignUpPage.css';
 

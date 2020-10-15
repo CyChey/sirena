62d6d4666a4865f331085f04fb25c95800239812
Cyrena Nielsen <cycheyenne@gmail.com>
2020-10-14 19:44:49 -0700

    deploy

--- config/auth.js
+++ config/auth.js
@@ -4,21 +4,25 @@ const SECRET = process.env.SECRET;
 module.exports = function (req, res, next) {
     // Check for the token being sent in three different ways
     let fullTokenString = req.get('Authorization') || req.query.token || req.body.token;
-    let parsedTokenString = fullTokenString.replace(/^(Bearer )/, '');
-    if (parsedTokenString) {
-        // Remove the 'Bearer ' if it was included in the token header
-
-        // Check if token is valid and not expired
-        jwt.verify(parsedTokenString, SECRET, function (err, decodedToken) {
-            if (err) {
-                next(err);
-            } else {
-                // It's a valid token, so add user to req
-                req.user = decodedToken.user;
-                next();
-            }
-        });
-    } else {
+    if (!fullTokenString) {
         next('No token sent');
+    } else {
+        let parsedTokenString = fullTokenString.replace(/^(Bearer )/, '');
+        if (parsedTokenString) {
+            // Remove the 'Bearer ' if it was included in the token header
+
+            // Check if token is valid and not expired
+            jwt.verify(parsedTokenString, SECRET, function (err, decodedToken) {
+                if (err) {
+                    next(err);
+                } else {
+                    // It's a valid token, so add user to req
+                    req.user = decodedToken.user;
+                    next();
+                }
+            });
+        } else {
+            next('No token sent');
+        }
     }
 };
